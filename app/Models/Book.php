77834d2c42edde4e344a0e997f2b40fa9007915c
Victor Alfonso <vc70383@hotmail.com>
2023-04-21 21:46:27 -0600

    Relaciones creadas

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+    public function students()
+    {
+        return $this->belongsToMany(Student::class, 'borrowings', 'book_id', 'student_id')
+            ->withPivot(['id', 'borrowed_at', 'returned_at'])
+            ->withTimestamps();
+    }
+
+
 }
--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    public function books()
+    {
+        return $this->belongsToMany(Book::class, 'borrowings', 'student_id', 'book_id')
+            ->withPivot(['id', 'borrowed_at', 'returned_at'])
+            ->withTimestamps();
+    }
 }
